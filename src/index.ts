3b67a5f532a12339c6119dc0a40d54de4810493e
oggnimodd <arnold.ds.dev@gmail.com>
2024-04-05 22:19:40 +0700

    base

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-import { sayHello } from "@/hello";
+import readline from "node:readline";
+import { resolve } from "node:path";
 
-const hello = sayHello();
+const run = (code: string) => {
+    console.log(code);
+};
 
-console.log(hello);
+const runFile = async (path: string) => {
+    const content = await Bun.file(resolve(process.cwd(), path)).text();
+    run(content);
+};
+
+const runPrompt = () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.prompt();
+
+    rl.on("line", (line) => {
+        run(line);
+        rl.prompt();
+    });
+
+    rl.on("close", () => {
+        console.log("\nCrox!");
+        process.exit(0);
+    });
+};
+
+const main = () => {
+    const args = process.argv.slice(2);
+
+    if (args.length > 1) {
+        throw new Error("Too many arguments");
+    }
+
+    if (args.length === 1) {
+        runFile(args[0]);
+    } else if (args.length === 0) {
+        runPrompt();
+    }
+};
+
+main();
